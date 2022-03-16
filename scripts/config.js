34d250123be96ae9b06b254fa9006fefcd005a3c
Conflict of Tanks of <101643711+ConflictOfTanks@users.noreply.github.com>
2022-03-16 09:05:58 +0000

    Login Popup Style Update

--- scripts/config.js
+++ scripts/config.js
@@ -1,11 +1,11 @@
-var gameTitle = "War of Tanks.io";
+var gameTitle = "Conflict of Tanks.io";
 var gameDescription = "War of Tanks.io is a competitive game! Compete with other tanks to obtain victory!";
 var playButton = "PLAY!";
 var gameDownText = "Game is currently under maintenance. Check us on <a href='/discord.html'>Discord</a> for more information!";
 var playerBanText = "This player has been banned.";
-var consoleHackWarning = "%c Please do not paste scripts from the internet. Hacking the game is against rules, and you might as well get banned from the game.";
-var consoleFact = "%c The console logs in-game actions! Only use this for executing actions (type \"cnsl('help')\" for help!) or to ask for help.";
-var rules = ["No hacking or using bots in any way.", "rule 2", "rule 3", "rule 4", "and so on"];
+var consoleHackWarning = "%c Please do not paste scripts from the internet. Hacking the game is against rules, and you might as well get banned from the game. There's a chance that you might get hacked instead if someone told you to paste something here.";
+var consoleFact = "%c The console logs are meant only for support purposes, such as asking for help.";
+var rules = ["No hacking or using bots in any way.", "No farming, or using alts to grind illegitimate statistics."];
 
 
 function loadConfig(param1) {

200c0abd9b26869f1c56a6a7ff1e466f22464036
Conflict of Tanks of <101643711+ConflictOfTanks@users.noreply.github.com>
2022-03-17 05:15:17 +0000

    memes be like

---
+++ scripts/lol.js
@@ -0,0 +1,20 @@
+// lol yeah
+// thank Fun X Time for making a cringe js file pls thx lmao
+
+// Conflict of FunXTime VS. Akari = COFVA
+var lmaoNicknames = document.getElementById("playerName");
+var lmaoNameThingy = "COFVA";
+var thisTimeLowercase = "cofva";
+var thisTimeNoobMobilePlayers = "Cofva";
+
+// lInEs YeS
+var lmao_line1 = "funxtime";
+var lmao_line2 = "vs";
+var lmao_line3 = "akari";
+
+
+function play() {
+  if (lmaoNickname == lmaoNameThingy || lmaoNickname == thisTimeLowercase || lmaoNickname == thisTimeNoobMobilePlayers) {
+    document.write(lmao_line1 + lmao_line2 + lmao_line3);
+  }
+}
